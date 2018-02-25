60e7fb1adc60309b7bc98d65dec889690d0c943c
Undead <undead@undead.xyz>
2018-02-25 17:28:53 -0600

    Code from trying to figure out where the file was exported to.

--- main.lua
+++ main.lua
@@ -22,6 +22,19 @@ function love.load(args)
 	for k,v in pairs(resultingIslands) do
 		table.insert(images, love.graphics.newImage(v))
 	end
+
+	renderedResult = renderResult(determineRenderSheetSize())
+	local fileData = renderedResult:newImageData():encode('png','export.png')
+
+	success, message = love.filesystem.write('export.png', fileData, fileData:getSize())
+
+	print(success, message)
+	-- love.event.quit()
+end
+
+function love.draw()
+	love.graphics.setBackgroundColor(128, 128, 128, 255)
+	love.graphics.draw(renderedResult)
 end
 
 function verifyArguments(args)
@@ -40,36 +53,6 @@ function verifyArguments(args)
 	local width = tonumber(args[3]) or 1024
 
 	return packingImage, width
-end
-
-function love.draw()
-	love.graphics.setBackgroundColor(255, 128, 128, 255)
-
-	local x, y = 0, 0
-	local offsetY = 0
-	local newY = 0
-	local sprite = 1
-	local spriteLimit = #images
-	while(sprite ~= spriteLimit) do
-		love.graphics.draw(images[sprite],x,y + offsetY)
-		x = x + images[sprite]:getWidth()
-
-		if(images[sprite]:getHeight() > newY)then
-			newY = images[sprite]:getHeight()
-		end
-
-		if(x >= width or ((sprite + 1 <= spriteLimit) and width - x < images[sprite + 1]:getWidth()))then
-			x = 0
-			y = offsetY + newY
-			offsetY = y
-			y = 0
-			newY = 0
-		end
-
-		sprite = sprite + 1
-	end
-
-	love.
 end
 
 function generateIslandPixelDump(imageData, x, y)
@@ -157,6 +140,64 @@ end
 
 function imageDataSortingHeuristic(imageDataA, imageDataB)
 	return imageDataA:getWidth() * imageDataA:getHeight() < imageDataB:getWidth() * imageDataB:getHeight()
+end
+
+function determineRenderSheetSize()
+	local x, y = 0, 0
+	local offsetY = 0
+	local newY = 0
+	local sprite = 1
+	local spriteLimit = #images
+	love.graphics.setCanvas(canvas)
+	while(sprite ~= spriteLimit) do
+		x = x + images[sprite]:getWidth()
+
+		if(images[sprite]:getHeight() > newY)then
+			newY = images[sprite]:getHeight()
+		end
+
+		if(x >= width or ((sprite + 1 <= spriteLimit) and width - x < images[sprite + 1]:getWidth()))then
+			x = 0
+			y = offsetY + newY
+			offsetY = y
+			y = 0
+			newY = 0
+		end
+
+		sprite = sprite + 1
+	end
+
+	return width, offsetY + newY
+end
+
+function renderResult(canvasWidth, canvasHeight)
+	local canvas = love.graphics.newCanvas(canvasWidth, canvasHeight)
+	local x, y = 0, 0
+	local offsetY = 0
+	local newY = 0
+	local sprite = 1
+	local spriteLimit = #images
+	love.graphics.setCanvas(canvas)
+	while(sprite ~= spriteLimit) do
+		love.graphics.draw(images[sprite],x,y + offsetY)
+		x = x + images[sprite]:getWidth()
+
+		if(images[sprite]:getHeight() > newY)then
+			newY = images[sprite]:getHeight()
+		end
+
+		if(x >= width or ((sprite + 1 <= spriteLimit) and width - x < images[sprite + 1]:getWidth()))then
+			x = 0
+			y = offsetY + newY
+			offsetY = y
+			y = 0
+			newY = 0
+		end
+
+		sprite = sprite + 1
+	end
+	love.graphics.setCanvas()
+	return canvas
 end
 
 function exportBitmap()
--- readme.md
+++ readme.md
@@ -9,3 +9,10 @@ run love lovetexturepacker imagehere width
 to generate an image of given width from the sprites contained in the image.
 
 Performance will likely be slow with large images, and the exported sheets are not efficiently packed.
+
+The result is exported to these directories:
+
+Windows XP 	C:\Documents and Settings\user\Application Data\LOVE\ 	%appdata%\LOVE\
+Windows Vista, 7, 8 and 10 	C:\Users\user\AppData\Roaming\LOVE 	%appdata%\LOVE\
+Mac 	/Users/user/Library/Application Support/LOVE/
+Linux 	$XDG_DATA_HOME/love/ 	~/.local/share/love/
